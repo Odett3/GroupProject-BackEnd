7196991a9ea5a91324cc10403f15cec3e6969328
Odett3 <67112041+Odett3@users.noreply.github.com>
2020-09-14 15:42:05 +0200

    added associations, i set up the migrations for the extra columns in summary

--- models/summary.js
+++ models/summary.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      summary.belongsTo(models.user), summary.hasOne(models.topic);
     }
   }
   summary.init(
