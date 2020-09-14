7c1e7e6613d1dec7b50693b8cd975d91c438e3a0
Odett3 <67112041+Odett3@users.noreply.github.com>
2020-09-14 16:12:20 +0200

    created new model for userTopic and added references

---
+++ migrations/20200914140528-create-user-topic.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('userTopics', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.INTEGER
+      },
+      topicId: {
+        type: Sequelize.INTEGER
+      },
+      isDone: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('userTopics');
+  }
+};
