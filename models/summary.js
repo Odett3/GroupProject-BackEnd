c95221b3d02f2712e653acc4018fc8b3cf4dd2a4
Odett3 <67112041+Odett3@users.noreply.github.com>
2020-09-14 20:50:05 +0200

    tested initial endpoints and fixed relation mistake

--- models/summary.js
+++ models/summary.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      summary.belongsTo(models.user), summary.hasOne(models.topic);
+      summary.belongsTo(models.user), summary.belongsTo(models.topic);
     }
   }
   summary.init(
