9111e5258226f17ebaa71a87f2e59d6e36038cf3
team-cake <eka.cheuk@gmail.com>
2020-09-18 14:06:57 +0200

    Added additional seed data for the userTopics

--- seeders/20200914154247-some-usertopics.js
+++ seeders/20200914154247-some-usertopics.js
@@ -1,37 +1,79 @@
-"use strict";
+'use strict'
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert(
-      "userTopics",
-      [
-        {
-          userId: 1,
-          topicId: 1,
-          isDone: false,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          userId: 2,
-          topicId: 2,
-          isDone: false,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          userId: 3,
-          topicId: 3,
-          isDone: false,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-      ],
-      {}
-    );
-  },
+	up: async (queryInterface, Sequelize) => {
+		await queryInterface.bulkInsert(
+			'userTopics',
+			[
+				{
+					userId: 1,
+					topicId: 1,
+					isDone: true,
+					createdAt: new Date(),
+					updatedAt: new Date(),
+				},
+				{
+					userId: 2,
+					topicId: 1,
+					isDone: true,
+					createdAt: new Date(),
+					updatedAt: new Date(),
+				},
+				{
+					userId: 3,
+					topicId: 1,
+					isDone: true,
+					createdAt: new Date(),
+					updatedAt: new Date(),
+				},
+				{
+					userId: 1,
+					topicId: 2,
+					isDone: true,
+					createdAt: new Date(),
+					updatedAt: new Date(),
+				},
+				{
+					userId: 2,
+					topicId: 2,
+					isDone: true,
+					createdAt: new Date(),
+					updatedAt: new Date(),
+				},
+				{
+					userId: 3,
+					topicId: 2,
+					isDone: false,
+					createdAt: new Date(),
+					updatedAt: new Date(),
+				},
+				{
+					userId: 1,
+					topicId: 3,
+					isDone: true,
+					createdAt: new Date(),
+					updatedAt: new Date(),
+				},
+				{
+					userId: 2,
+					topicId: 3,
+					isDone: false,
+					createdAt: new Date(),
+					updatedAt: new Date(),
+				},
+				{
+					userId: 3,
+					topicId: 3,
+					isDone: false,
+					createdAt: new Date(),
+					updatedAt: new Date(),
+				},
+			],
+			{}
+		)
+	},
 
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkDelete("userTopics", null, {});
-  },
-};
+	down: async (queryInterface, Sequelize) => {
+		await queryInterface.bulkDelete('userTopics', null, {})
+	},
+}
